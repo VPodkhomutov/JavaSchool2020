7ef5ea47cc14f0aa2a5c9713228a4773b34c02ef
Slava <tanzors@mail.ru>
2020-11-19 21:43:42 +0300

    1 урок

--- src/slava/Main.java
+++ src/slava/Main.java
@@ -3,5 +3,10 @@ package slava;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int i =1;
+        int y= 2;
+
+        System.out.println(i+y);
     }
 }
