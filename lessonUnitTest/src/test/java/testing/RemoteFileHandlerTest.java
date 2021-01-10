8a2ed900610ab131af679c54f8c9f4522384b713
Slava <tanzors@mail.ru>
2021-01-10 16:43:31 +0300

    урок по логированию и тестированию. исправил замечания, но остались вопросы

--- lessonUnitTest/src/test/java/testing/RemoteFileHandlerTest.java
+++ lessonUnitTest/src/test/java/testing/RemoteFileHandlerTest.java
@@ -4,48 +4,44 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.*;
 
+import static com.sun.javaws.JnlpxArgs.verify;
 import static org.mockito.ArgumentMatchers.any;
-import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(RemoteFileHandler.class)
 public class RemoteFileHandlerTest {
     @Mock
     SettingsLoader settingsLoader;
     @Mock
     ResponseRepository responseRepository;
     @Mock
-    SettingsLoader settingsLoader2;
-    @Mock
-    ResponseRepository responseRepository2;
-    @Mock
     RemoteFileReadWriter remoteFileReadWriter;
     @Mock
     RemoteFileReadWriter rFRW;
     @Mock
-    public RemoteFileHandler rFH;
-    @Mock
-    public RemoteFileHandler rFH2;
+    RemoteFileHandler rFH;
 
     /*handleError*/
     @Test
     public void handleErrorIsNullList() {
         List<String> tmp =null;
         RemoteFileHandler r = new RemoteFileHandler(settingsLoader,responseRepository);
-        Assert.assertEquals(false, r.handleError(tmp));
+        Assert.assertFalse(r.handleError(tmp));
     }
 
     @Test
     public void handleErrorTrue() {
         List<String> tmp = Arrays.asList("One","Two");
         RemoteFileHandler r = new RemoteFileHandler(settingsLoader,responseRepository);
-        Assert.assertEquals(true, r.handleError(tmp));
+        Assert.assertTrue(r.handleError(tmp));
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -58,7 +54,7 @@ public class RemoteFileHandlerTest {
     /*handleRequest*/
 
     @Test
-    public void handleRequestTestTrue() {
+    public void handleRequestTestTrue() throws Exception {
         Map<String,String> sett = new HashMap<>();
         ArrayList<String> al = new ArrayList<>();
         sett.put("one","one");
@@ -66,8 +62,12 @@ public class RemoteFileHandlerTest {
         RemoteFileHandler r = new RemoteFileHandler(settingsLoader,responseRepository);
         when(settingsLoader.loadSettings()).thenReturn(sett);
         //тут ошибка которая говорит что тест написан неправильно
-        when(rFRW.readFileToList(sett)).thenReturn(al);
+        //when(rFRW.readFileToList(sett)).thenReturn(al);
+        //надо заменить на следующие 2 строчки
+        RemoteFileReadWriter fileReadWriter = mock(RemoteFileReadWriter.class);
+        PowerMockito.whenNew(RemoteFileReadWriter.class).withNoArguments().thenReturn(fileReadWriter);
         r.handleRequest();
+        //verify(responseRepository).writeResult(al); //вроде правильно написал, но выдает ошибку
     }
 
     @Test
@@ -88,18 +88,22 @@ public class RemoteFileHandlerTest {
         Boolean b1 = false;
         List<String> tmp = Arrays.asList("One","error");
         //rfh.handleResponse(tmp);
-        RemoteFileHandler r2 = new RemoteFileHandler(settingsLoader2,responseRepository2);
-        when(rFH2.validate(any())).thenReturn(b1);
-        r2.handleResponse(tmp);
+        RemoteFileHandler r = new RemoteFileHandler(settingsLoader,responseRepository);
+        when(rFH.validate(any())).thenReturn(b1);
+        r.handleResponse(tmp);
     }
 
     @Test
-    public void handleResponseTestTrueValidate() {
+    public void handleResponseTestTrueValidate() throws Exception {
         Boolean b = true;
         List<String> tmp = Arrays.asList("One","error");
         RemoteFileHandler r = new RemoteFileHandler(settingsLoader,responseRepository);
         when(rFH.validate(any())).thenReturn(b);
         when(responseRepository.writeResult(tmp)).thenReturn(b);
+        //теоретически здесь тоже должны быть эти 2 строки
+        //но почему-то тут ошибка пропала скорее всего стал использоваться ранее описанный mock
+        //RemoteFileReadWriter fileReadWriter = mock(RemoteFileReadWriter.class);
+        //PowerMockito.whenNew(RemoteFileReadWriter.class).withNoArguments().thenReturn(fileReadWriter);
         r.handleResponse(tmp);
     }
 }
